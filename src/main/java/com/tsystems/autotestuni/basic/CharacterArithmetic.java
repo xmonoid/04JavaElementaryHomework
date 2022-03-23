bf70f3997e8aa6d603d199a57e1136defbc7a977
Evgenii Kosykh <Evgenii.Kosykh@t-systems.com>
2022-03-23 19:19:41 +0300

    Added basic tasks

---
+++ src/main/java/com/tsystems/autotestuni/basic/CharacterArithmetic.java
@@ -0,0 +1,29 @@
+package com.tsystems.autotestuni.basic;
+
+/**
+ * To practice with characters and inequalities.
+ *
+ * @author ekosykh
+ */
+public class CharacterArithmetic {
+
+    /**
+     * Check that the input symbols is a decimal digit.
+     *
+     * @param symbol input symbol
+     * @return {@code true} if the symbol is a digit, {@code false} otherwise
+     */
+    public static boolean checkSymbolIsDigit(char symbol) {
+        return false; // FIXME: Implement code here
+    }
+
+    /**
+     * Check that the input symbols is a latin letter in upper or lower case.
+     *
+     * @param symbol input symbol
+     * @return {@code true} if the symbol is a latin letter, {@code false} otherwise
+     */
+    public static boolean checkSymbolIsLatinLetter(char symbol) {
+        return false; // FIXME: Implement code here
+    }
+}
---
+++ src/main/java/com/tsystems/autotestuni/basic/TemperatureScaleConverter.java
@@ -0,0 +1,29 @@
+package com.tsystems.autotestuni.basic;
+
+/**
+ * To practice with decimals.
+ *
+ * @author ekosykh
+ */
+public final class TemperatureScaleConverter {
+
+    /**
+     * Convert temperature from Fahrenheit to Celsius scale.
+     *
+     * @param fahrenheit temperature in Fahrenheit
+     * @return temperature in Celsius
+     */
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return Double.NaN; // FIXME: Implement code here
+    }
+
+    /**
+     * Convert temperature from Celsius to Fahrenheit scale.
+     *
+     * @param celsius temperature in Celsius
+     * @return temperature in Fahrenheit
+     */
+    public static double celsiusToFahrenheit(double celsius) {
+        return Double.NaN; // FIXME: Implement code here
+    }
+}
